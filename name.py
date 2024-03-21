ebf020a5d1755ef204dbebddee796b20fde5770b
Ajobin1 <164325996+Ajobin1@users.noreply.github.com>
2024-03-21 14:38:31 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,7 @@
+dict1={'Arigato':'Thank you','Daijob':'Alright','Wakatha':'Ok','Ekemesho':'Lets go',"Onichaan":'Bigbro'}
+for d in dict1 :
+  print(d,"==>",dict1[d])
+print(dict1.keys())
+print(dict1.values())
+print(dict1.items())
+print("Kerala"not in dict1)
